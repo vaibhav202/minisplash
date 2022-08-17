ef14ec6c8ebbaea0b2ea0edc7dd5aa793ed2da44
Vaibhav Pancholi <71831734+vaibhavpancholi05@users.noreply.github.com>
2022-08-18 01:49:22 +0530

    Uploaded files via GitHub desktop

---
+++ phone/abstract/main.js
@@ -0,0 +1,29 @@
+const displayImages = document.querySelector(".boxes");
+const miniSplash = 'https://api.unsplash.com/search/photos/?per_page=30&order_by=latest&orientation=portrait&query=abstract&client_id=jNPKpYi80xqW7F7Ot37Mqsaft-NsbggphFw0WRAyw-E';
+fetch(miniSplash)
+.then(response =>{
+    if(response.ok){
+        return response.json();
+    }
+    else{
+        alert(response.status);
+    }
+})
+.then(data =>{
+    for(i = 0 ; i < data.results.length; i++){
+        var images = document.createElement("img");
+        var imagesDownloadLink = document.createElement("a");
+        Object.assign(imagesDownloadLink, {
+            href: data.results[i].urls.raw,
+            target: '_blank'
+        });
+        imagesDownloadLink.download;
+        imagesDownloadLink.setAttribute('data-scroll' , '');
+        imagesDownloadLink.appendChild(images);
+        imagesDownloadLink.classList.add('box');
+        images.setAttribute('src' , data.results[i].urls.regular);
+        displayImages.appendChild(imagesDownloadLink);
+        // images.style.backgroundImage = 'url('+data.results[i].urls.raw+')';
+        // images.innerHTML = '<img src='+data.results[i].urls.raw+'>';
+    }
+});
