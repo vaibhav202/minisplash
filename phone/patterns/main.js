7b50429cda4ddf0c5059e2aa4cefcc9234c3ef09
Vaibhav Pancholi <71831734+vaibhavpancholi05@users.noreply.github.com>
2022-08-21 02:36:14 +0530

    Design, Grid layout and some other changes...
    From now 50 images will be fetched in all categories.

--- phone/patterns/main.js
+++ phone/patterns/main.js
@@ -1,5 +1,5 @@
 const displayImages = document.querySelector(".boxes");
-const miniSplash = 'https://api.unsplash.com/search/photos/?per_page=30&order_by=latest&orientation=portrait&query=patterns&client_id=jNPKpYi80xqW7F7Ot37Mqsaft-NsbggphFw0WRAyw-E';
+const miniSplash = 'https://api.unsplash.com/search/photos/?per_page=50&order_by=latest&orientation=portrait&query=patterns&client_id=jNPKpYi80xqW7F7Ot37Mqsaft-NsbggphFw0WRAyw-E';
 fetch(miniSplash)
 .then(response =>{
     if(response.ok){
